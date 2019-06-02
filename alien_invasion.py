c20af63d8c4e5fc9d1c6df361240128efa18648f
pancongwen <peter.pancongwen@gmail.com>
2019-06-02 23:36:32 +0800

    feat: refound

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,22 +1,30 @@
 import sys
 import pygame
-from settings import Settings
 
-def run_game():
-    pygame.init()
-    ai_setting = Settings()
-    screen = pygame.display.set_mode((ai_setting.screen_width, ai_setting.screen_height))
-    pygame.display.set_caption(ai_setting.caption)
-    
-    bg_color = ai_setting.bg_color
+class AlienInvasion():
+    """
+    """
+    def __init__(self):
+        """
+        """
+        self.WindowWeight = 1200
+        self.WindowHeight = 800
+        self.WindowTitle = "Alien Invasion"
 
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
+    def run(self):
+        """
+        """
+        pygame.init()
+        pygame.display.set_mode((self.WindowWeight, self.WindowHeight))
+        pygame.display.set_caption(self.WindowTitle)
 
-        screen.fill(bg_color)
-        pygame.display.flip()
 
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            pygame.display.flip()
 
-run_game()
+if __name__ == "__main__":
+    GAME = AlienInvasion()
+    GAME.run()
