bcc52b288d0b8e79a9e205cff71f1e6897e17301
pancongwen <peter.pancongwen@gmail.com>
2019-06-09 14:32:26 +0800

    feat: separete setting module

---
+++ setting.py
@@ -0,0 +1,12 @@
+
+class Setting():
+    """
+    """
+    def __init__(self):
+        """
+        """
+        self.WindowWeight = 1200
+        self.WindowHeight = 800
+        self.WindowTitle = "Alien Invasion"
+        self.WindowColor = (230, 230, 230)
+    
