d10d2128aa5d2591689bff484cedef7264b1a617
pancongwen <peter.pancongwen@gmail.com>
2019-06-12 22:23:47 +0800

    feat: separete event keyup and keydown check

--- game_function.py
+++ game_function.py
@@ -8,16 +8,26 @@ def check_events(ship):
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                ship.MovingRight = True
-            elif event.key == pygame.K_LEFT:
-                ship.MovingLeft = True
+            check_keydown_event(event, ship)
             
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                ship.MovingRight = False
-            elif event.key == pygame.K_LEFT:
-                ship.MovingLeft = False
+            check_keyup_event(event, ship)
+
+def check_keyup_event(event, ship):
+    """
+    """
+    if event.key == pygame.K_RIGHT:
+        ship.MovingRight = False
+    elif event.key == pygame.K_LEFT:
+        ship.MovingLeft = False
+
+def check_keydown_event(event, ship):
+    """
+    """
+    if event.key == pygame.K_RIGHT:
+        ship.MovingRight = True
+    elif event.key == pygame.K_LEFT:
+        ship.MovingLeft = True
 
 def update_screen(setting, screen, ship):
     """
